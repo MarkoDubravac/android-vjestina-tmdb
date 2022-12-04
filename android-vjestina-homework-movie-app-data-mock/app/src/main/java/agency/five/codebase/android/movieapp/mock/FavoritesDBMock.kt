5734d9927ce1108a32c9b937a7a80d88b9f9a34e
marko <markodubravac123@gmail.com>
2022-12-04 21:57:19 +0100

    Added flows, fake repository, DI, ViewModels, not finished

---
+++ android-vjestina-homework-movie-app-data-mock/app/src/main/java/agency/five/codebase/android/movieapp/mock/FavoritesDBMock.kt
@@ -0,0 +1,18 @@
+package agency.five.codebase.android.movieapp.mock
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+object FavoritesDBMock {
+
+    val favoriteIds = MutableStateFlow(setOf<Int>());
+
+    fun insert(movieId: Int){
+        favoriteIds.value.plusElement(movieId)
+
+    }
+    fun delete(movieId: Int){
+        favoriteIds.value.minusElement(movieId)
+    }
+
+}
